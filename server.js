8ba1c77afbd66cb87a86a4c189fb838820c91fe2
shoaib shahid <shoaibshahid.smit@gmail.com>
2026-04-19 03:56:12 +0500

    Backend Error Solve

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(cors({
   origin: [
     process.env.FRONTEND_URL, 
     "http://localhost:5173",
+    "http://localhost:5174",
     "http://localhost:5175",
     "https://final-frontend-three-peach.vercel.app"
   ], 
